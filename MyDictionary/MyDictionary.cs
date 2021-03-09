9ad6188bac418c00375e4c58b57ccf56e892e20d
mtarikg <mtarikgol97@gmail.com>
2021-03-10 02:27:05 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary <T1, T2>
+    {
+        T1[] keys;
+        T2[] values;
+
+        public MyDictionary()
+        {
+            keys = new T1[0];
+            values = new T2[0];
+        }
+
+        public void Add(T1 element, T2 item)
+        {
+            T1[] tempKeys = keys;
+            T2[] tempValues = values;
+
+            keys = new T1[keys.Length + 1];
+            values = new T2[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+            }
+
+            for (int i = 0; i < tempValues.Length; i++)
+            {
+                values[i] = tempValues[i];
+            }
+
+            keys[keys.Length - 1] = element;
+            values[values.Length - 1] = item;
+        }
+
+        public int MyKeysLength {
+            get { return keys.Length; } 
+        }
+
+        public int MyValuesLength { 
+            get
+            {
+                return values.Length;
+            } 
+        }
+        public void ShowData()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine("ID: " + keys[i] + " Language: " + values[i]);
+            }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> languages = new Dictionary<int, string>();
+            languages.Add(1, "English");
+            languages.Add(2, "Turkish");
+            languages.Add(3, "German");
+            Console.WriteLine("Dictionary Counter: " + languages.Count);
+            Console.WriteLine();
+            Console.WriteLine("Content of the dictionary: ");
+
+            foreach (KeyValuePair<int, string> kvp in languages)
+            {
+                Console.WriteLine("ID: " + kvp.Key + " Language: " + kvp.Value);
+            }
+
+            Console.WriteLine();
+
+            MyDictionary<int, string> myLanguages = new MyDictionary<int, string>();
+            myLanguages.Add(1, "English");
+            myLanguages.Add(2, "Turkish");
+            myLanguages.Add(3, "German");
+            Console.WriteLine("MyDictionary - Key Counter: " + myLanguages.MyKeysLength);
+            Console.WriteLine("MyDictionary - Value Counter: " + myLanguages.MyValuesLength);
+            Console.WriteLine();
+            Console.WriteLine("Content of the dictionary: ");
+
+            myLanguages.ShowData();
+        }
+    }
+}
